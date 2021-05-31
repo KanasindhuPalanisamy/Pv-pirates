80acfe8ac53d6237fe5b1a3a225fea53b4563aad
KanasindhuPalanisamy <84837922+KanasindhuPalanisamy@users.noreply.github.com>
2021-05-31 19:52:44 +0530

    Update Maintest.js

--- Maintest.js
+++ Maintest.js
@@ -1,4 +1,33 @@
 import {MainPage} from '../integration/my_first_test'
+import {ManageOrg} from '../integration/ManageOrg'
+import { Points_holders } from '../integration/Points_holders'
+
+
+const mainPage=new MainPage()
+const manageorg=new ManageOrg()
+const pointsholders=new Points_holders()
+
+
+
+// it('login test',function(){
+    mainPage.routepaths();
+//     mainPage.navigate();
+//     mainPage.sidebar();
+//     // mainPage.DashboardGrid();
+// })
+
+
+// it('test_manageorg',function(){
+//         dashboard.navigate();
+//         manageorg.tabs();
+//         manageorg.sponsor()
+    
+//  })
+
+it('point_holders',function(){
+    mainPage.navigate();
+    pointsholders.table();
+})import {MainPage} from '../integration/my_first_test'
 import { Orgoverview } from './Orgoverview';
 
 const mainPage=new MainPage()
